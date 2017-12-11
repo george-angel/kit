2ba4a2e2da97bd605beae28b5f0cfa974bb0363d
George Angel <george@infosecproject.org>
2017-12-11 09:28:59 +0000

    add dev box

---
+++ aethon/main.sh
@@ -0,0 +1,1 @@
+include docker-deb.sh
---
+++ aethon/main.tf
@@ -0,0 +1,31 @@
+variable "role" {
+  default = "aethon"
+}
+
+variable "instance_count" {
+  default = 1
+}
+
+data "scaleway_image" "debian-stretch" {
+  architecture = "x86_64"
+  name         = "Debian Stretch"
+}
+
+resource "scaleway_security_group" "sg" {
+  count       = "${var.instance_count}"
+  name        = "${var.role}-${count.index}"
+  description = "${var.role} ${count.index}"
+}
+
+resource "scaleway_server" "server" {
+  count               = "${var.instance_count}"
+  name                = "${var.role}-${count.index}"
+  image               = "${data.scaleway_image.debian-stretch.id}"
+  type                = "C2L"
+  dynamic_ip_required = true
+  security_group      = "${element(scaleway_security_group.sg.*.id, count.index)}"
+
+  provisioner "local-exec" {
+    command = "mkdir -p inventory/ && echo ${self.public_ip} ${var.role} ${count.index} >> inventory/${var.role}"
+  }
+}
