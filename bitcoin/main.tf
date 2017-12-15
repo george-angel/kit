eeff8122d1140693999ec3ce1f78ba60e75d3d7c
George Angel <github-057ad5e1-988d-4a79-9156-864d85dfb1fb@infosecproject.org>
2017-12-15 22:46:39 +0000

    working btc node

--- bitcoin/main.tf
+++ bitcoin/main.tf
@@ -21,11 +21,21 @@ resource "scaleway_server" "server" {
   count               = "${var.instance_count}"
   name                = "${var.role}-${count.index}"
   image               = "${data.scaleway_image.debian-stretch.id}"
-  type                = "VC1S"
+  type                = "C2S"
   dynamic_ip_required = true
   security_group      = "${element(scaleway_security_group.sg.*.id, count.index)}"
 
+  volume {
+    size_in_gb = 150
+    type       = "l_ssd"
+  }
+
+  volume {
+    size_in_gb = 150
+    type       = "l_ssd"
+  }
+
   provisioner "local-exec" {
-    command = "echo ${self.public_ip} ${var.role} ${count.index} >> ${var.role}-inv"
+    command = "mkdir -p inventory/ && echo ${self.public_ip} ${var.role} ${count.index} > inventory/${var.role}"
   }
 }
