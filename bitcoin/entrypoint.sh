a446b12828016aca2cfb53f5974b43a6f7342ecc
George Angel <github-7574494a@infosecproject.org>
2018-01-13 14:05:05 +0000

    fmt + index

--- bitcoin/entrypoint.sh
+++ bitcoin/entrypoint.sh
@@ -2,4 +2,8 @@
 
 chown -R bitcoin:bitcoin /bitcoin/data || fail "Cannot change ownership of '/bitcoin/data'."
 
-exec gosu bitcoin /usr/bin/bitcoind -datadir=/bitcoin/data -disablewallet -printtoconsole
+exec gosu bitcoin /usr/bin/bitcoind \
+ -datadir=/bitcoin/data \
+ -disablewallet \
+ -txindex=1 \
+ -printtoconsole
