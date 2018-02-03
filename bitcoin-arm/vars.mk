b539bb0a425b73ce2f7736db0a8d3c333c59aac5
George Angel <github-7574494a@infosecproject.org>
2018-02-03 22:14:44 +0000

    consolidated into a single module

--- bitcoin-arm/vars.mk
+++
@@ -1,3 +0,0 @@
-# STRONGBOX ENCRYPTED RESOURCE ; See https://github.com/uw-labs/strongbox
-Bln5Nms1szoKNnN/9UueDeEoTWNpAZ9JoplCZ4mRyHYNbR/hVyKZv/Q9OH9pmlmcFCWmIvyy0Exa
-alpNL/eHdbk/8IbEEJn5bXu31vk3IaVy28c=
