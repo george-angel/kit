6460d8685b55ab46d1964806973fe6c2fee40653
George Angel <github-7574494a@infosecproject.org>
2018-09-30 10:39:39 +0100

    clean

--- _lib/bork/common.mk
+++ _lib/bork/common.mk
@@ -7,20 +7,26 @@ ssh_opts=-oStrictHostKeyChecking=no
 # $4 arch
 
 _setup:
-	ssh $(ssh_opts) $(user)@$(IP) apt -y update
-	ssh $(ssh_opts) $(user)@$(IP) apt -y install git
+	ssh $(ssh_opts) $(user)@$(IP) apt-get -y update
+	ssh $(ssh_opts) $(user)@$(IP) apt-get -y install git
 	ssh $(ssh_opts) $(user)@$(IP) "test -d /usr/local/src/bork || git clone https://github.com/mattly/bork /usr/local/src/bork"
 	ssh $(ssh_opts) $(user)@$(IP) "test -L /usr/local/bin/bork || ln -sf /usr/local/src/bork/bin/bork /usr/local/bin/bork"
 	ssh $(ssh_opts) $(user)@$(IP) "test -L /usr/local/bin/bork-compile || ln -sf /usr/local/src/bork/bin/bork-compile /usr/local/bin/bork-compile"
 
+_update:
+	ssh $(ssh_opts) $(user)@$(IP) apt-get -y update
+
 _upgrade:
-	$(ssh)$(IP) apt -y upgrade
+	ssh $(ssh_opts) $(user)@$(IP) apt-get -y upgrade
 
 trust-all:
 	cat inventory/* | awk '{print $$1}' | parallel ssh-keyscan {} >> ~/.ssh/known_hosts
 
 setup-all:
 	cat inventory/* | awk '{print $$1}' | parallel make _setup IP={} user=root
+
+update-all:
+	cat inventory/* | awk '{print $$1}' | parallel make _update IP={}
 
 upgrade-all:
 	cat inventory/* | awk '{print $$1}' | parallel make _upgrade IP={}
@@ -30,6 +36,3 @@ satisfy-all:
 
 status-all:
 	cat inventory/* | awk '{print "make _satisfy IP="$$1" ROLE="$$2" NUM="$$3" ARCH="$$4" ACTION=status"}' | parallel
-
-restart-node-all:
-	cat inventory/* | awk '{print $$1}' | parallel $(ssh){} sudo systemctl reboot
