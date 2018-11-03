0512ad23bd54a3ed8a39e15ff1f3359dd8792b79
George Angel <github-7574494a@infosecproject.org>
2018-11-03 17:36:07 +0000

    start move bork => ansible
    - tor

--- tor/vars.mk
+++
@@ -1,3 +0,0 @@
-# STRONGBOX ENCRYPTED RESOURCE ; See https://github.com/uw-labs/strongbox
-VO4IGEc+GAlQtO5jvaW/SgeKr1xXYJmGGlNsTRwpETadIvCh3etj03YaZ2qd6zEvn9yuSFUDtyCw
-7+FdLUTfD3wf81lRyKsq/PWifCanXrRE0BSz0zIai7oVuvhYZnJjeVy8LbQz3mqcIKyUgQ==
