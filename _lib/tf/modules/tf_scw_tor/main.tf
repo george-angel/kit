2d1306d02297b4e36837b2fe11040169b0dc2ce5
George Angel <github-7574494a@infosecproject.org>
2020-04-27 15:34:12 +1000

    Remove redundant comment for Tor terraform

--- _lib/tf/modules/tf_scw_tor/main.tf
+++ _lib/tf/modules/tf_scw_tor/main.tf
@@ -19,8 +19,6 @@ resource "scaleway_instance_server" "server" {
 
   lifecycle {
     ignore_changes = [
-      # Ignore changes to tags, e.g. because a management agent
-      # updates these based on some ruleset managed elsewhere.
       image,
       user_data,
     ]
